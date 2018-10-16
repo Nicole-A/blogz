385a0d0c0dd0e6bd0c450735a7de3fc60bae33c1
Nicole <nicole_a_alejandro@live.com>
2018-10-16 17:21:53 -0400

    Completed functionality. Need to add styling.

--- main.py
+++ main.py
@@ -23,21 +23,40 @@ class Blog(db.Model):
 def index():
 
     blog_title = request.args.get('blog_title')
-    blogs = Blog.query.all()
+    blog_id = request.args.get('id')
 
-    return render_template('blog.html',title="Build a Blog", blogs=blogs)
+    if not blog_id:
+        blogs = Blog.query.all()
+        return render_template('blog.html',title="Build a Blog", blogs=blogs)
+    else:
+        blogs = Blog.query.get(blog_id)
+        return render_template('display.html', title= "Blog Post",blogs=blogs)
+    
 
 
 @app.route('/newpost', methods=['POST', 'GET'])
 def post_entry():
+
     if request.method == 'POST': 
+        title_error = ''
+        entry_error = ''
         blog_title = request.form['blog_title']
-        blog_entry = request.form['body']  
-        new_blog = Blog(blog_title,blog_entry)
-        db.session.add(new_blog)
-        db.session.commit()
-        return redirect('/blog')
+        blog_entry = request.form['body']
     
+        if not blog_title:
+            title_error = 'Enter a title'
+            
+        if not blog_entry:
+            entry_error = 'Entry cannot be blank'
+
+        if title_error or entry_error:
+            return render_template('new_post.html',title="New Entry", blog_entry=blog_entry, title_error=title_error, entry_error=entry_error)
+        else:     
+            new_blog = Blog(blog_title,blog_entry)
+            db.session.add(new_blog)
+            db.session.commit()
+            return redirect('/blog?id='+str(new_blog.id))
+        
     return render_template('new_post.html',title="New Entry")
 
 
