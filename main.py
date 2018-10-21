6ca58082b8bf77143400233635c6255ab2022869
Nicole <nicole_a_alejandro@live.com>
2018-10-20 23:15:54 -0400

    Added loging and signup template. SQL error when creating user.

--- main.py
+++ main.py
@@ -13,20 +13,20 @@ class Blog(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(120))
     body = db.Column(db.String(500))
-    owner_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    #owner_id = db.Column(db.Integer, db.ForeignKey('user.id'))
 
     def __init__(self, title, body, owner):
         self.title = title
         self.body = body
-        self.owner_id = owner
+        #self.owner_id = owner
 
 
 class User(db.Model):
 
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(120))
+    username = db.Column(db.String(120), unique=True)
     password = db.Column(db.String(20))
-    blogs = db.relationship('Blog', backref='id')
+    #blogs = db.relationship('Blog', backref='id')
 
     def __init__(self,username, password):
         self.username = username
@@ -75,6 +75,27 @@ def post_entry():
         
     return render_template('new_post.html',title="New Entry")
 
+@app.route('/login', methods=['POST', 'GET'])
+def login():
+    if request.method == 'POST' :
+        username = request.form['username']
+        password = request.form['password']
+        user = User.query.filter_by(username=username).first()
+        
+        if user and user.password == password:
+            #remember user login
+            return redirect ('/')
+        else:
+            #login fialed message
+            pass
+
+    return render_template('login.html')
+
+
+
+@app.route('/signup', methods=['POST', 'GET'])
+def signup():
+    return render_template('signup.html')
 
 if __name__ == '__main__':
     app.run()
